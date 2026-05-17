bd3c4945e72da9670481f8b3faa149a51adf3be7
amanuniyal01 <amanuniyal315@gmail.com>
2026-05-17 09:49:08 +0530

    Learned Arrays and Tuples

--- src/basic.ts
+++ src/basic.ts
@@ -1,6 +1,15 @@
 // Primitives - That store single values.
 let message: string = "Hello TypeScript";
-let age:number=19;
-let isValidToVote:boolean=true;
-console.log(message,age,isValidToVote);
+let age: number = 19;
+let isValidToVote: boolean = true;
+console.log(message, age, isValidToVote);
 
+// Arrays
+let fruits: string[] = ["Apple", "Banana", "Mango"];
+let count:number[]=[1,2,3,4,5];
+let mixArray=[1,2,3,"Aman","Anurag"];
+console.log(fruits,count,mixArray)
+
+// Tuples
+let person:[string,number]=["Aman",21]
+console.log(person)
