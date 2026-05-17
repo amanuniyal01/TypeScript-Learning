8acc044613e819868df55f883bf60ccb8de9efda
amanuniyal01 <amanuniyal315@gmail.com>
2026-05-17 10:32:39 +0530

    Functions and Type of Functions

---
+++ src/function.ts
@@ -0,0 +1,13 @@
+// number -returns number.
+function add(a:number,b:number):number{//this number is return type..
+    return a+b
+}
+console.log(add(5,3))
+
+// Void -returns nothing.
+function info(name:string):void{
+    console.log("Hello"+name)
+
+}
+info("Aman")
+
