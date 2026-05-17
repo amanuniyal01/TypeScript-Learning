66b9098756f246a60b37af59dedc88392b33bdad
amanuniyal01 <amanuniyal315@gmail.com>
2026-05-17 09:54:57 +0530

    Learned enum,any and unknown type

--- src/basic.ts
+++ src/basic.ts
@@ -6,10 +6,24 @@ console.log(message, age, isValidToVote);
 
 // Arrays
 let fruits: string[] = ["Apple", "Banana", "Mango"];
-let count:number[]=[1,2,3,4,5];
-let mixArray=[1,2,3,"Aman","Anurag"];
-console.log(fruits,count,mixArray)
+let count: number[] = [1, 2, 3, 4, 5];
+let mixArray = [1, 2, 3, "Aman", "Anurag"];
+console.log(fruits, count, mixArray);
 
 // Tuples
-let person:[string,number]=["Aman",21]
-console.log(person)
+let person: [string, number] = ["Aman", 21];
+console.log(person);
+
+// ENUM
+enum Color {
+  Red = "Red",
+  Blue = "Blue",
+}
+let favcolor: Color = Color.Blue;
+console.log(favcolor);
+
+// Any type(avoid id possible)
+let randomValue:any=10
+randomValue="Any type"
+
+// Unknown(safer than any)
