e5e4f3337500373df05f2662346edd0f01fc1a30
amanuniyal01 <amanuniyal315@gmail.com>
2026-06-13 12:58:44 +0530

    Created basic counter ui

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,7 +1,14 @@
 
 function Counter() {
     return (
-        <div className="">Counter</div>
+        <div className="flex flex-col items-center justify-center">
+            <h1 className="font-bold">Counter App</h1>
+            <div className="flex gap-5 mt-10">
+                <button className="bg-gray-200 rounded-lg p-3">Increment</button>
+                <button className="bg-gray-200 rounded-lg p-3">Decrement</button>
+                <button className="bg-gray-200 rounded-lg p-3">Reset</button>
+            </div>
+        </div>
     )
 }
 
